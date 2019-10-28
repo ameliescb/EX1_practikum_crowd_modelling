eb64d1dd424b7e99ce971ba192c26bc844147772
Cyberlilie <56976782+Cyberlilie@users.noreply.github.com>
2019-10-28 20:53:03 +0100

    Add files via upload
    Reorganisation of the code by task : one task per file

---
+++ Task2ex1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 28 20:40:03 2019
+
+@author: amelie
+"""
+
+import matplotlib.pyplot as plt
+from tkinter import * 
+import numpy as np
+import tkinter.font as tkFont
+import tkinter as tk
+from ex1_functions import *
+
+#%%
