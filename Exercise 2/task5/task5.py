43b6a87e5b99f360f734c507eaf4bb0ca600663f
Yalvac Top <yalvac.top@siemens.com>
2019-11-13 20:10:05 +0100

    uploaded outputs of GNM and OSM

--- Exercise 2/task5/task5.py
+++ Exercise 2/task5/task5.py
@@ -2,7 +2,17 @@ import json
 import os
 import glob
 
-#removes the source field from the actual scenario and saves the new scenario in order to use it for the next timesteps
+def getPedoLocations(postvis_path):
+
+	pedos = {}
+	#finds the most recently created file and reads it line by line
+	postvis = open(glob.glob(postvis_path)[-1], "r")
+	for line in postvis:
+		l_arr = line.split()
+		pedos[l_arr[1]] = [l_arr[2], l_arr[3]]
+
+	return pedos_location # a dict with key value pedestrian id and value as an array of coordinates [x, y]
+
 def removeSourceField(path_of_scenario_file, path_to_new_scenario):
 
 	with open(path_of_scenario_file, 'r') as f:
@@ -12,7 +22,7 @@ def removeSourceField(path_of_scenario_file, path_to_new_scenario):
 	with open(path_to_new_scenario, 'w') as outfile:
 	    json.dump(scenario, outfile, indent=2)
 
-#runs the scenerio
+
 def runScenario(path_of_vadere, path_of_scenario, path_of_output):
 
 	run_scenario = 'java -jar '+ path_of_vadere + ' scenario-run --scenario-file "' + path_of_scenario + '" --output-dir="' + path_of_output + '"'
@@ -56,7 +66,8 @@ def addPedos(path_of_scenario, path_of_output, path_of_postvis):
 	with open(path_of_postvis, 'w') as outfile:
 	    json.dump(scenario, outfile, indent=2)
 
-#returns the number of pedestrians who could not reach the target
+
+
 def countPedos(path_of_output):
 
 	pedos = {}
@@ -70,12 +81,11 @@ def countPedos(path_of_output):
 
 '''
 1 pedestrians are created in the source field
-1 they get out of the source field at the end of time 1
-2 remove the source field of the scenario and save it as task5_changed
-3 take the location of pedestrians from the output of the most recently ran scenario
-4 put the location of pedestrians in the scenario created at the previous iteration (task5_changed)
-5 run the scenario again using task5_changed.scenario
-repeat from 2 until time is out
+2 they get out of the source field at the end of time 1
+3 delete the source field
+4 take their locations
+5 put them locations in the same scenario and run one more time1
+repeat from 3 until time is out
 '''
 
 def task4():
@@ -84,11 +94,15 @@ def task4():
 	time = 0
 
 	runScenario("vadere/vadere-console.jar", "practikum/Exercise 2/task5/scenarios/task5.scenario", "output")
+	runScenario("vadere/vadere-console.jar", "practikum/Exercise 2/task5GNM/scenarios/task5.scenario", "outputGNM")
 
 	while (time < max_finish_time):
 		removeSourceField("practikum/Exercise 2/task5/scenarios/task5.scenario", "task5_changed.scenario")
+		removeSourceField("practikum/Exercise 2/task5GNM/scenarios/task5.scenario", "task5GNM_changed.scenario")
 		addPedos("task5_changed.scenario", "output/task5_*/postvis.trajectories", "task5_changed.scenario")
+		addPedos("task5GNM_changed.scenario", "outputGNM/task5_*/postvis.trajectories", "task5GNM_changed.scenario")
 		runScenario("vadere/vadere-console.jar", "task5_changed.scenario", "output")
+		runScenario("vadere/vadere-console.jar", "task5GNM_changed.scenario", "outputGNM")
 
 		time += 1
 
