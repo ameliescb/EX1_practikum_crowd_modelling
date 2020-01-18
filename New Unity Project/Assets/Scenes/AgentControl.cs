712351d9ff6e778efb053c0436f85d449af504ac
Amélie Sciberras <amelie.sciberras@telecom-paristech.fr>
2020-01-18 12:28:37 +0100

    obstacle avoidance and clicking on a target

--- New Unity Project/Assets/Scenes/AgentControl.cs
+++ New Unity Project/Assets/Scenes/AgentControl.cs
@@ -6,12 +6,33 @@ using UnityEngine.AI;
 public class AgentControl : MonoBehaviour
 {
     public Transform home;
+    public Camera cam;
     NavMeshAgent agent;
-
+    
     // Start is called before the first frame update
-    void Start()
+  //  void Start()
+   // {
+        
+     //   agent.SetDestination(home.position);
+      //  agent.speed = 50 ; 
+   // }
+
+    void Update()
     {
         agent = this.GetComponent<NavMeshAgent>();
-        agent.SetDestination(home.position);
+        if (Input.GetMouseButtonDown(0))
+        {
+           Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+           RaycastHit hit ;
+
+           if (Physics.Raycast(ray,out hit)) 
+           {
+               // move the agent
+               agent.SetDestination(hit.point) ; 
+           }
+        }
+        agent.speed = 10 ; 
+
+
     }
 }
