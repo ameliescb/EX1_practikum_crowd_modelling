40b0a0e36d361742e37e2769d596f57627b1f1e8
Yalvac Top <yalvac.top@siemens.com>
2020-01-19 16:22:09 +0100

    update

---
+++ New Unity Project/Assets/Scenes/SpawnObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour
+{
+    public GameObject agent;
+    public Vector3 center;
+    public Vector3 size;
+    public Quaternion rotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnPedo();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnPedo()
+    {
+        Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.y / 2, size.y / 2), Random.Range(-size.z / 2, size.z / 2));
+
+        Instantiate(agent, pos, Quaternion.identity);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1,0,0,0.5f);
+        Gizmos.DrawCube(center, size);
+    }
+}
