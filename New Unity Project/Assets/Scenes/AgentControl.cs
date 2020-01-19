cf7a041a7f3f8b38252ba8bd60b644f56382ea86
Amélie Sciberras <amelie.sciberras@telecom-paristech.fr>
2020-01-19 17:44:21 +0100

    Input fields connected and functionnal

--- New Unity Project/Assets/Scenes/AgentControl.cs
+++ New Unity Project/Assets/Scenes/AgentControl.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using System ; 
 
 public class AgentControl : MonoBehaviour
 {
     public Transform home;
     public Camera cam;
     NavMeshAgent agent;
-   
+    int speed = 0; 
+    int pedNum = 0; 
+
+
 
     void Update()
     {
         agent = this.GetComponent<NavMeshAgent>();
+      //  speedInput.ActivateInputField ();
         if (Input.GetMouseButtonDown(0))
         {
            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
@@ -28,18 +33,26 @@ public class AgentControl : MonoBehaviour
 
     }
 
+    int getSpeed() {
+        return this.speed ;
+    }
+
+    int getPedNum() {
+        return this.pedNum ;
+    }
+
     public void BeginSimulation() {
-        agent = this.GetComponent<NavMeshAgent>();
-        Debug.Log("begin simulation clicked") ;
+        // start simulation using speed and pednum 
     } 
 
     public void SpeedChange(string newSpeed) {
-         Debug.Log("speed has been changed : ") ;
-         Debug.Log(newSpeed) ;
+        this.speed = int.Parse(newSpeed) ;
    } 
 
-   public void pedNumChange(string pedNum) {
-         Debug.Log("number of pedestrians has been changed : ") ;
-         Debug.Log(pedNum) ;
+   public void pedNumChange(string newPedNum) {
+
+       this.pedNum = int.Parse(newPedNum) ;
+
+
    } 
 }
